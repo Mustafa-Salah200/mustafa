d4daa1b63747e23702015baab1b6004e5cfe16a5
Mustafa-Salah200 <mustafaslahtoofy@gmail.com>
2025-06-27 15:59:58 -0400

    update

---
+++ src/Components/Allprojects/Allprojects.jsx
@@ -0,0 +1,19 @@
+import './Allprojects.css'
+import p1 from "../../assets/projects/p1.png";
+import dan from "../../assets/projects/p2.png";
+import p3 from "../../assets/projects/p3.png";
+import p4 from "../../assets/projects/p4.png";
+import emergencies from "../../assets/projects/p5.png";
+import p6 from "../../assets/projects/p6.png";
+import files_management from "../../assets/projects/p8.png";
+import quiz_app from "../../assets/projects/p9.png";
+import ui_ux_Profile from "../../assets/projects/p10.png";
+
+
+const Allprojects = () => {
+  return (
+    <div>Allprojects</div>
+  )
+}
+
+export default Allprojects
